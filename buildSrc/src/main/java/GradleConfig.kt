dff4473c294df691eb0d7a69452394835af92c62
Herman <chummer1008@gmail.com>
2025-06-17 15:21:52 +0300

    Update comments

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -8,9 +8,9 @@ object GradleConfig {
     val javaVersion = JavaVersion.VERSION_1_8
     const val jvmTarget = "1.8"
 
-    const val namespaceRoot = "com.chummer.template"
+    const val namespaceRoot = "com.chummer.template" // TODO replace template value
 
     const val versionName = "1.0.0"
 
-    const val dbName = "myDb" // TODO configure
+    const val dbName = "myDb" // TODO replace template value
 }
