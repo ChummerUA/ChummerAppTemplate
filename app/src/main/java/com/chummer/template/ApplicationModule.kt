82ff76f886777e8040881bf8e9b8e12617679721
Herman <chummer1008@gmail.com>
2025-06-17 15:10:39 +0300

    Fix ApplicationModule

--- app/src/main/java/com/chummer/template/ApplicationModule.kt
+++ app/src/main/java/com/chummer/template/ApplicationModule.kt
@@ -4,9 +4,10 @@ import com.chummer.db.DbModule
 import com.chummer.network.NetworkModule
 import com.chummer.preferences.PreferencesModule
 import dagger.Module
-import javax.inject.Singleton
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-@Singleton
+@InstallIn(SingletonComponent::class)
 @Module(includes = [NetworkModule::class, DbModule::class, PreferencesModule::class])
 object ApplicationModule {
 }
